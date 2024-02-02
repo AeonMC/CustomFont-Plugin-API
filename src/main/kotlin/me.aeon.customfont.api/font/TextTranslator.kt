bf30ecdc23186a475e5051bf9015bc640c42e69a
Aeon <ignoramexmf@gmail.com>
2024-02-02 17:33:36 +0100

    Initial

--- src/main/kotlin/me.aeon.customfont.api/font/TextTranslator.kt
+++ src/main/kotlin/me.aeon.customfont.api/font/TextTranslator.kt
@@ -8,7 +8,8 @@ interface TextTranslator {
     fun translate(text: String, font: CustomFont): String
     fun translate(text: String, font: CustomFont, viewer: OfflinePlayer?, parseBracketPlaceholders: Boolean): String
     fun translateAndCache(text: String, font: CustomFont, viewer: OfflinePlayer?): String
-    fun untranslate(text: String, font: CustomFont): String
+    /* Not implemented yet */
+    // fun untranslate(text: String, font: CustomFont): String
 
     companion object : TextTranslator by TextTranslatorImpl.Implementation
 
