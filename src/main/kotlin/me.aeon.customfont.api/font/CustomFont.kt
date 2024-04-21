27fe5e58e016de69d7f224bc1cce7a07ddba4be7
Aeon <ignoramexmf@gmail.com>
2024-04-21 16:19:24 +0200

    Native version 1.3.0

--- src/main/kotlin/me.aeon.customfont.api/font/CustomFont.kt
+++ src/main/kotlin/me.aeon.customfont.api/font/CustomFont.kt
@@ -8,13 +8,9 @@ interface CustomFont : Font {
     class Builder : AbstractBuilder by CustomFontImpl.Builder()
 
     interface AbstractBuilder {
-        var id: String
-        var permission: String?
-        var characters: MutableMap<CharacterType, String?>
-
         fun id(fontId: String): AbstractBuilder
         fun permission(permission: String?): AbstractBuilder
-        fun characters(characterType: CharacterType, characters: String): AbstractBuilder
+        fun characters(chars: Pair<String, String>): AbstractBuilder
         fun build(): CustomFont
     }
 
--- src/main/kotlin/me.aeon.customfont.api/font/Font.kt
+++ src/main/kotlin/me.aeon.customfont.api/font/Font.kt
@@ -4,7 +4,7 @@ import me.aeon.customfont.plugin.font.DefaultFont
 
 interface Font {
     val id: String
-    val characters: String
+    val characters: Pair<String, String>
     val translationMap: Map<Char, String>
     fun setupTranslationMap(): Map<Char, String>
 
