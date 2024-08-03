6fdb380dfa26910e2aec379629de0d24ef4ef132
Aeon <ignoramexmf@gmail.com>
2024-08-03 15:09:17 +0200

    Add options to add and save fonts

--- src/main/kotlin/xyz/aeonxd/customfont/api/font/FontManager.kt
+++ src/main/kotlin/xyz/aeonxd/customfont/api/font/FontManager.kt
@@ -1,10 +1,12 @@
 package xyz.aeonxd.customfont.api.font
 
+import xyz.aeonxd.customfont.api.CustomFontPluginAPI
+
 abstract class FontManager {
 
-    abstract val fontMap: Map<String, CustomFont>
-    abstract val fontIds: List<String>
-    abstract val fonts: List<CustomFont>
+    val fontMap: Map<String, CustomFont> by lazy { loadFonts() }
+    val fontIds: List<String> by lazy { fontMap.keys.toList() }
+    val fonts: List<CustomFont> by lazy { fontMap.values.toList() }
 
     /**
      * Whether to use case sensitivity when looking
@@ -18,10 +20,23 @@ abstract class FontManager {
     abstract fun loadFonts(): Map<String, CustomFont>
 
     /**
-     * Returns a [CustomFont], otherwise `null`
+     * Returns a [CustomFont] if it exists, otherwise `null`.
      *
      * Takes [useCaseSensitivity] into consideration
      */
     abstract fun fromId(id: String): CustomFont?
+
+    /**
+     * Writes the [font] to fonts.yml
+     * Save the fonts with [saveNewFonts] and then reload the plugin
+     * with [CustomFontPluginAPI.reload] to retrieve them with FontManager.
+     */
+    abstract fun addFont(font: CustomFont): Boolean
+
+    /**
+     * Saves the newly written fonts from [addFont]. Reload
+     * the plugin with [CustomFontPluginAPI.reload] to retrieve them with FontManager.
+     */
+    abstract fun saveNewFonts(): Boolean
 
 }
